20501b96821fcfcf519e5c9257dfecb87805eea1
Jonathan Doctolero <jonathan.doctolero7@gmail.com>
2020-01-19 06:54:39 -0800

    1/2 way done with choice logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ var player1;
 var player2;
 var choice1;
 var choice2;
+var rps1Array = ["rock", "paper", "scissors"];
+var rps2Array = ["rock2", "paper2", "scissors2"];
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -27,16 +29,22 @@ function setUpRoom() {
     } else {
         database.ref(roomCode).once("value", function(snapshot) {
             console.log(snapshot);
-            if (!snapshot.child("player1ID").exists()) {
+            if (!snapshot.child("Player1").exists()) {
                 player1 = roomCode + "1";
-                database.ref(roomCode).update({
-                    player1ID: player1
+                player2 = null;
+                database.ref(roomCode + '/Player1').update({
+                    player1ID: player1,
+                    choice: 'empty'
                 });
-            } else if (!snapshot.child("player2ID").exists()) {
+                alert("Hello, Player 1");
+            } else if (!snapshot.child("Player2").exists()) {
                 player2 = roomCode + "2";
-                database.ref(roomCode).update({
-                    player2ID: player2
+                player1 = null;
+                database.ref(roomCode + '/Player2').update({
+                    player2ID: player2,
+                    choice: 'empty'
                 });
+                alert("Hello, Player 2");
             } else {
                 alert("Sorry, room full");
             }
@@ -44,12 +52,33 @@ function setUpRoom() {
     }
 }
 
+
 $("#roomCodeBtn").on("click", function(event) {
     event.preventDefault();
     setUpRoom();
 })
 
 $(".choice").on("click", function() {
-    console.log($(this).attr("id"));
-    console.log(player1, player2);
+    var choice = $(this).attr("id");
+    if (player1 != null) {
+        if (rps1Array.includes(choice)) {
+            console.log(player1, choice);
+            database.ref(roomCode + '/Player1').update({
+                choice: choice
+            });
+            $(this).addClass("active");
+        } else {
+            console.log("Cannot choose for player 2");
+        }
+    } else if (player2 != null) {
+        if (rps2Array.includes(choice)) {
+            console.log(player2, choice);
+            database.ref(roomCode + '/Player2').update({
+                choice: choice
+            });
+            $(this).addClass("active");
+        } else {
+            console.log("Cannot choose for player 1");
+        }
+    }
 })
